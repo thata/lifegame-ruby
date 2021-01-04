7061b690ac111244ba53bcfd74407ef085995ac2
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2021-01-05 00:31:00 +0900

    リファクタ

--- 1000_generation.rb
+++ 1000_generation.rb
@@ -2,18 +2,19 @@
 
 require './lib/life_game'
 
-trap(:INT) do
-  exit
-end
-
-# ブロック内の処理を計測
-StackProf.run(out: '/tmp/stackprof.dump') do
+def start_game
   height, width = `stty size`.split.map(&:to_i)
   game = LifeGame.new(height, width)
 
+  # 1000世代実行して終了する
   1000.times do
     print "\e[1;1H"
     game.dump_cell
     game.next_generation
   end
+end
+
+# ブロック内の処理を計測
+StackProf.run(out: '/tmp/stackprof.dump') do
+  start_game
 end
