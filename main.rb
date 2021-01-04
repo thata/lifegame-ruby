72136b45e95456ad99b1151222db2864212c443f
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2021-01-04 20:38:33 +0900

    libへ移動

---
+++ main.rb
@@ -0,0 +1,17 @@
+# bundle exec ruby main.rb
+
+require './lib/life_game'
+
+height, width = `stty size`.split.map(&:to_i)
+game = LifeGame.new(height, width)
+
+trap(:INT) do
+  exit
+end
+
+while true
+  print game.dump_cell
+  print "\e[1;1H"
+  game.next_generation
+  sleep 0.1
+end
